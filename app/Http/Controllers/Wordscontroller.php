4d324ad751cb5d2827c7f78c91736d62fde5f771
yutaro704 <yutaro.704@gmail.com>
2020-01-16 19:14:28 +0900

    create Words controller
    create Words controller and add routing @index to web.php

---
+++ app/Http/Controllers/Wordscontroller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Wordscontroller extends Controller
+{
+    public function index(){
+        return "test";
+    }
+}
