f00d864ece164c4b84bdd8f0b25857d5d1d04849
yutaro704 <yutaro.704@gmail.com>
2020-01-19 20:37:28 +0900

    fix show.blade.php
    fix show.blade.php

--- resources/views/words/show.blade.php
+++ resources/views/words/show.blade.php
@@ -1,13 +1,5 @@
-<!DOCTYPE html>
-<html lang='ja'>
-<head>
-  <meta charset="utf-8">
-  <title>{{ $word->en }}</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-<body>
-  <div class="container">
-    <h1>{{ $word->en }}</h1>
-  </div>
-</body>
-</html>
+@extends('layouts.default')
+@section('title', 'Myword')
+@section('content')
+<h1>{{ $word->en }}</h1>
+@endsection
