686bcdf9421e0cce5a27b8e50201b658170b77d3
yutaro704 <yutaro.704@gmail.com>
2020-01-21 18:27:01 +0900

    add association to User.php and Word.php

--- app/User.php
+++ app/User.php
@@ -36,4 +36,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function words(){
+        return $this->hasMany('App\Word');
+    }
 }
