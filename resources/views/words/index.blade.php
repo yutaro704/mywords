6595df72d1f9bcf0181afdd36e8c5f4918ed1479
yutaro704 <yutaro.704@gmail.com>
2020-01-28 19:00:46 +0900

    fix UI index.blade.php

--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -3,22 +3,21 @@
 @section('content')
 <h1>
   <a href="{{url('/words/create')}}" class="header-menu" >新しいワードを追加する</a>
-  <a href="#" class="header-menu">Mywordsとは？<a>
 </h1>
     <ul class="words">
       @foreach($words as $word)
       <li class="word">
-      <a href="#" class="del" data-id="{{ $word->id }}">単語を削除する</a>
-        <div class="en">
-          <a href="{{ action('WordsController@show', $word )}}">{{ $word->en }}</a>
-        </div>
-        <b class="etranslation">和訳</b>
-          <a href="/" class="ja">{{ $word->ja }}</a>
-        <b class="partofspeech">品詞</b>
-        <form method="post" action="{{ url('/words', $word->id) }}" id="form_{{ $word->id }}">
+      <a href="{{ action('WordsController@show', $word )}}" class="show">例文をみる</a>
+      <a href="{{ action('WordsController@edit', $word) }}" class='edit'>編集する</a>
+      <a href="#" class="del" data-id="{{ $word->id }}">削除する</a>
+      <form method="post" action="{{ url('/words', $word->id) }}" id="form_{{ $word->id }}">
           {{ csrf_field() }}
           {{ method_field('delete') }}
         </form>
+      <div class="en">{{ $word->en }}</div>
+        <b class="etranslation">和訳</b>
+          {{ $word->ja }}
+        <b class="partofspeech">品詞</b>
       </li>
       @endforeach
     </ul>
