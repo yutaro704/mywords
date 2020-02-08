1abee9c783540202233847a17c0a25542f5aac4b
yutaro704 <yutaro.704@gmail.com>
2020-02-08 14:01:31 +0900

    fix index and show blade.php

--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -2,20 +2,17 @@
 @section('title', 'Myword')
 @section('content')
 <div class=add>
-  <a href="{{url('/words/create')}}" class="add_word" >単語を追加</a>
+  <a href="{{url('/words/create')}}" class="add_word" >質問する</a>
 </div>
     <ul class="words">
       @foreach($words as $word)
       <li class="word">
-      <div class="en">
-        <a href="{{ action('WordsController@show', $word )}}" class="show">{{ $word->en }}</a>
+      <div class="titlequestion">
+        <a href="{{ action('WordsController@show', $word )}}" class="show">{{ $word->title }}</a>
       </div>
-        <b class="etranslation">和訳</b>
-          {{ $word->ja }}
-        <b class="partofspeech">品詞</b>
-          {{ $word->wordclass }}
-        <b class="partofspeech">投稿者</b>
+      <b class="partofspeech">投稿者</b>
         {{ Auth::user()->name }}
+        <!-- ↑ログインしている状態のユーザーの表示をするように設定してあるので、投稿ユーザーがログインしていない状態だとトップページがエラーになる。 -->
       </li>
       @endforeach
     </ul>
