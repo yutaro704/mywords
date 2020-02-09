348cf85dbe4aab6f4b1a4687815c10695295a7ca
yutaro704 <yutaro.704@gmail.com>
2020-02-09 11:47:50 +0900

    add association to model files

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+    protected $fillable = ['body'];
+
+    public function user(){
+        return $this->belongsTo('App\User');
+    }
+    public function word() {
+        return $this->belongsTo('App\Word');
+    }
 }
