7186e4fd607b05f017023c917df92bd0d837ac1c
yutaro704 <yutaro.704@gmail.com>
2020-02-10 10:10:02 +0900

    add text of top picture to index.blade.php

--- resources/views/words/index.blade.php
+++ resources/views/words/index.blade.php
@@ -1,6 +1,18 @@
 @extends('layouts.app')
 @section('title', 'Myword')
 @section('content')
+<div top_pic>
+  <img class="top_pic__logo" src="{{ asset('images/top.png') }}" alt="top_logo">
+  <div class="top_pic__title">
+    古都グアナファクトとその銀鉱群
+  </div>
+  <div class="top_pic__text">
+    メキシコ中央部高原の中にある比較的小さな街です。<br>
+    地元の人も自慢するだけあり、町中から丘の上まで広がる<br>
+    コロニアル調の家々がびっしりと描かれた街の景色です。<br>
+    丘の上からの街の眺めが有名で、街歩きをすることで色々満喫つできる街です。
+  </div>
+</div>
 <div class=add>
   <a href="{{url('/words/create')}}" class="add_word" >質問する</a>
 </div>
