1b0b0a5b519cc74030936c26dc39a9250f57d367
yutaro704 <yutaro.704@gmail.com>
2020-02-10 20:59:15 +0900

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">新規登録・ログイン完了</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,10 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    ログインが完了しました
+                    <a class= "navbar-brand" href="{{ url('/') }}">
+                    Mywordsをはじめる
+                    </a>
                 </div>
             </div>
         </div>
