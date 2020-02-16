d2356baeeab4dac393aa0d0c15c436aa06273765
yutaro704 <yutaro.704@gmail.com>
2020-02-16 13:27:22 +0900

    fix fillable word.php

--- app/Word.php
+++ app/Word.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Word extends Model
 {
     //
-    protected $fillable = ['title', 'body', 'user_id', 'wordclass'];
+    protected $fillable = ['title', 'body', 'user_id'];
 
     public function user(){
         return $this->belongsTo('App\User');
