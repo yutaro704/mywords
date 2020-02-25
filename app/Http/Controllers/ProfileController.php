ab5c3acebba856aa290c52bc732ef06af4d04f63
yutaro704 <yutaro.704@gmail.com>
2020-02-25 17:29:46 +0900

    update index

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,10 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProfileRequest;
+use Auth;
+use Storage;
 
 class ProfileController extends Controller
 {
+    /**
+     *
+     * @param ProfileRequest $request
+     * @return Response
+     */
+
     public function index(){
-        return view('profile/index');
+        $image = false;
+        
+        if (Storage::disk('local')->exists('public/profile_images/' . Auth::id() . '.jpg')) {
+            $image = true;
+        }
+        return view('profiles/index');
     }
 }
